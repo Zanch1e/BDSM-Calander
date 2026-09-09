b8802d14c39b1917a5f30e37031acb461002854a
Zanch1e <kingrosenstand@gmail.com>
2026-09-09 16:07:04 +0200

    Update allowed nicknames in FirebaseConnector.js

--- FirebaseConnector.js
+++ FirebaseConnector.js
@@ -37,7 +37,7 @@ loginForm.addEventListener('submit', (e) => {
     const nickname = nicknameInput.value.trim().toLowerCase();
 
     // 1. DEFINE YOUR TWO ALLOWED NICKNAMES HERE (Must be lowercase)
-    const allowedNicknames = ["name1", "name2"];
+    const allowedNicknames = ["herrer", "slave"];
 
     // 2. CHECK IF THE TYPED NICKNAME IS ON THE ALLOWED LIST
     if (!allowedNicknames.includes(nickname)) {
